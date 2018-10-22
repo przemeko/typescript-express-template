f38a5dbcfa4bfa1ba8ebdbaef70eaf5488fdbfc6
Przemyslaw Kocznur <pkocznur@gmail.com>
2018-10-22 15:19:37 +0200

    Add cors and helmet middleware

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import http from 'http';
 import apiRouter from './api/api.router';
 import Container from './core/container';
+import cors from 'cors';
+import helmet from 'helmet';
 
 export default class Server {
 
   constructor(readonly container: Container) {
+    this.setupMiddleware();
     this.container.app.use('/api', apiRouter(this.container));
+  }
+
+  private setupMiddleware() {
+    this.container.app.use(cors());
+    this.container.app.use(helmet());
   }
 
   public listen(): http.Server {
